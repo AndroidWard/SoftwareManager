fdeec5c65b096db4276c3d52a0767cdb043933ec
wudih146 <wudih146126@163.com>
2015-03-04 08:02:16 +0800

    SoftManager
    软件管理器

---
+++ src/com/example/softmanager/Main.java
@@ -0,0 +1,15 @@
+package com.example.softmanager;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Main extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
