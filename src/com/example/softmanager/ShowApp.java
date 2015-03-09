2e2eb40fa45e89496a9f573a28dffe5ccb83d81a
wudih146 <wudih146126@163.com>
2015-03-09 19:06:07 +0800

    布局修改
    主界面布局、列表布局和网格布局

--- src/com/example/softmanager/ShowApp.java
+++ src/com/example/softmanager/ShowApp.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class Main extends Activity {
+public class ShowApp extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+		setContentView(R.layout.showapp);
 	}
 }
